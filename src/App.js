fcb0178e9358e718594aa3b608b1e8f99e53dd23
Sonir Aster <sunwolf114@gmail.com>
2023-09-05 15:40:50 +0300

    created app template

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+import styles from './App.css';
+import React from 'react';
+
+class App extends React.Component{
+    state = { details: [], }
+
+    componentDidMount(){
+        let data;
+        axios.get('http://localhost:8000')
+        .then(res => {
+            data = res.data;
+            this.setState({
+                details: data
+            });
+        })
+        .catch(err => {
+            console.log(err);
+        })
+        // data =  '1';
+        // console.log(data);
+    }
+    render() {
+        return (
+            <div>
+                <span>New Test App</span>
+                {this.state.details.map((output, id) => (
+                    <div key={id}>
+                        <h2 className={styles.dd}>{output.title}</h2>
+                        <p>{output.channel}</p>
+                    </div>
+                ))}
+            </div>
+        )
+    }
+
+}
+
+export default App;
+
