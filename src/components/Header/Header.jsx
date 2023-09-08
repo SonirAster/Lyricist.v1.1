5cd409a4955ba8bb1a9e00305f921e30b49d1f55
Sonir Aster <sunwolf114@gmail.com>
2023-09-08 12:03:21 +0300

    Merge branch 'main' of https://github.com/SonirAster/Lyricist.v1.0

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './Header.module.css';
+
+const Header = () => {
+    return (
+        <div className={s.header}>
+            Find any group or song You seek for!
+        </div>
+    )
+}
+
+export default Header;
