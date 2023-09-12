24025bfebf74c3df5a87a6a54fc596f295cc342b
SerafimaAminnoko <132136596+SerafimaAminnoko@users.noreply.github.com>
2023-09-12 13:30:45 +0300

    added new view

--- backend/song_project/songs/views.py
+++ backend/song_project/songs/views.py
@@ -3,16 +3,20 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework import viewsets, mixins
 from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
+from rest_framework.viewsets import GenericViewSet
 
 from .models import *
 from .serializers import GroupSerializer
 
 
-@csrf_exempt
-def groupAPI(request):
-    if request.method == 'GET':
-        groups = Group.objects.all()
-        groups_serializer = GroupSerializer(groups, many=True)
-        return JsonResponse(groups_serializer.data, safe=False)
+class GroupViewSet(GenericViewSet,
+                   mixins.ListModelMixin):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+
 
